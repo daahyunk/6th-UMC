948795d7a07259dae87ac768bcd520fdd56606f4
daahyunk <ekgusdl0408@naver.com>
2024-07-02 17:53:11 +0900

    ✨ 카카오 공유 기능 추가
    카카오 공유 기능 추가

--- week10-1/src/api/ShareKakao.jsx
+++ week10-1/src/api/ShareKakao.jsx
@@ -38,18 +38,18 @@ const ShareKakao = () => {
         content: {
             title: '영화 검색 사이트', // 카카오톡 제목
             description: '#UMC #6기 #한성대 #Web #10주차',  // 카카오톡 상세 설명
-            imageUrl:'https://ibb.co/sqGknh7',    // 카카오톡 이미지
+            imageUrl:'https://i.ibb.co/1mT4x6H/IMG-3778-1.png',    // 카카오톡 이미지
             link: {
-            mobileWebUrl: 'hhttps://umc-web-week10.netlify.app/', // 배포 링크
-            webUrl: 'https://umc-web-week10.netlify.app/',   // 배포 링크
+            mobileWebUrl: 'https://umc-movie-week10.netlify.app/', // 배포 링크
+            webUrl: 'https://umc-movie-week10.netlify.app/',   // 배포 링크
             },
         },
         buttons: [
             {
             title: '영화 찾아보러 가기',    // 카카오톡 버튼
             link: {
-                mobileWebUrl: 'https://umc-web-week10.netlify.app/', // 배포 링크
-                webUrl: 'https://umc-web-week10.netlify.app/',   // 배포 링크
+                mobileWebUrl: 'https://umc-movie-week10.netlify.app/', // 배포 링크
+                webUrl: 'https://umc-movie-week10.netlify.app/',   // 배포 링크
             },
             },
         ],
